dd14f1a58d15a07b186f893ec42b90aba2e192fa
sudip3711 <149355694+sudip3711@users.noreply.github.com>
2025-04-28 21:07:47 +0530

    8

--- Student_Mental_Health/settings.py
+++ Student_Mental_Health/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/' # below this add the following line
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'userApp/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
